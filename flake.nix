6ee221c1fe920ad3cc048c8b8980e7994856a80e
Sean Allesina-McGrory <allesis@wwu.edu>
2026-02-18 06:03:32 -0800

    Update nix files
    Add script to get unique values from a column in a csv file.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- flake.nix
+++ flake.nix
@@ -27,16 +27,60 @@
                 awk -v FS="\t" -v OFS="," '{for (i=1;i<=NF;i++) {x=gensub(/"/,"\"\"","g",$i); if (x ~ /"/ || x ~ /,/) $i="\""x"\""; else $i=$i}} 1' "$1" | sed "s/\r$//" > "$(echo "$1" | xargs -n 1 basename | sed "s/\..*//")".csv;
               '';
             };
+          get-unique-column-values-from-csv = pkgs.writeShellApplication {
+            name = "get-unique-column-values-from-csv";
+            runtimeInputs = [];
+            text = ''
+              # Thanks: https://gist.github.com/thomas-optimove/2616bd242be059fb2ec7cd1762946b15
+              # Check for the correct number of arguments
+              if [[ $# -ne 2 ]]; then
+               echo "Usage: $0 <csv_file> <column_number>"
+               exit 1
+              fi
+
+              # Arguments
+              CSV_FILE="$1"
+              COLUMN_NUMBER="$2"
+
+              # Check if the file exists
+              if [[ ! -f "$CSV_FILE" ]]; then
+               echo "Error: File '$CSV_FILE' not found!"
+               exit 1
+              fi
+
+              # Check if the column number is valid (greater than 0)
+              if [[ ! "$COLUMN_NUMBER" =~ ^[0-9]+$ ]] || [[ "$COLUMN_NUMBER" -lt 1 ]]; then
+               echo "Error: Column number must be a positive integer."
+               exit 1
+              fi
+
+              # Extract the specified column, remove the header, and get unique values
+              awk -F, -v column_number="$COLUMN_NUMBER" '
+              NR == 1 {
+               # Ensure the column number is valid by checking the number of fields
+               if (column_number > NF) {
+                   print "Error: Column number " column_number " is out of range."
+                   exit 1
+               }
+               next
+              }
+              {
+               print $column_number
+              }' "$CSV_FILE" | sort | uniq
+            '';
+          };
         in
           f {
             pkgs = pkgs;
             tsv2csv = tsv2csv;
+            get-unique-column-values-from-csv = get-unique-column-values-from-csv;
           }
       );
   in {
     devShells = forEachSupportedSystem ({
       pkgs,
       tsv2csv,
+      get-unique-column-values-from-csv,
     }: {
       default = pkgs.mkShell {
         packages = with pkgs; [
@@ -46,6 +90,7 @@
           just
           just-lsp
           tsv2csv
+          get-unique-column-values-from-csv
         ];
       };
     });
