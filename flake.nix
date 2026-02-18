583ab09e84bd4b84f27c23c7438b19ee82d54533
Sean Allesina-McGrory <allesis@wwu.edu>
2026-02-17 19:08:22 -0800

    Update nix files
    Add a script which converts a tsv to a csv using a script from:
    https://www.datafix.com.au/BASHing/2021-10-13.html
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- flake.nix
+++ flake.nix
@@ -11,24 +11,41 @@
   }: let
     supportedSystems = ["x86_64-linux" "x86_64-darwin"];
     forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
+      nixpkgs.lib.genAttrs supportedSystems (
+        system: let
           pkgs = import nixpkgs {
             inherit system;
           };
-        });
+          tsv2csv =
+            pkgs.writeShellApplication
+            {
+              name = "tsv2csv";
+              runtimeInputs = [pkgs.gawk];
+              text = ''
+                # Thanks: https://www.datafix.com.au/BASHing/2021-10-13.html
+                set -eu
+                awk -v FS="\t" -v OFS="," '{for (i=1;i<=NF;i++) {x=gensub(/"/,"\"\"","g",$i); if (x ~ /"/ || x ~ /,/) $i="\""x"\""; else $i=$i}} 1' "$1" | sed "s/\r$//" > "$(echo "$1" | xargs -n 1 basename | sed "s/\..*//")".csv;
+              '';
+            };
+        in
+          f {
+            pkgs = pkgs;
+            tsv2csv = tsv2csv;
+          }
+      );
   in {
-    devShells = forEachSupportedSystem ({pkgs}: {
+    devShells = forEachSupportedSystem ({
+      pkgs,
+      tsv2csv,
+    }: {
       default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-        ];
-
         packages = with pkgs; [
           uv
           python313Packages.jedi-language-server
           black
           just
           just-lsp
+          tsv2csv
         ];
       };
     });
