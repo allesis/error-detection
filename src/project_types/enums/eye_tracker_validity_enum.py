f632b5b8288a2279f59b80a44c01665a9a98960d
Sean Allesina-McGrory <allesis@wwu.edu>
2026-02-25 16:36:21 -0800

    Fix EyeTrackerValidityEnum definition
    Now works correctly with upper and lowercase strings
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- src/project_types/enums/eye_tracker_validity_enum.py
+++ src/project_types/enums/eye_tracker_validity_enum.py
@@ -1,10 +1,6 @@
 from enum import Enum
 
 
-class EyeTrackerValidityEnum(Enum):
-    PARTIAL = "partial", 1
-    WHOLE = "whole", 2
-
-    @classmethod
-    def from_str(self, s) -> EyeTrackerEventEnum:
-        return self(s.strip().lower())
+class EyeTrackerValidityEnum(str, Enum):
+    PARTIAL = "partial"
+    WHOLE = "whole"
