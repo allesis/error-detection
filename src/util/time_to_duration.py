a61c7eaf4d398ed73dc2d4dd511cc539dd2982b3
Sean Allesina-McGrory <allesis@wwu.edu>
2026-02-25 16:38:27 -0800

    Update time_to_duration to use regex groups
    Also add a catchall for trailing chars since occasionally we get input
    of the form HH:MM:SS.MS
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- src/util/time_to_duration.py
+++ src/util/time_to_duration.py
@@ -5,7 +5,7 @@ from project_types.errors.regex_match_error import RegexMatchError
 
 @type_enforced.Enforcer(enabled=True, strict=True, clean_traceback=True)
 def time_to_duration(time_str: str) -> int:
-    _TIME_REGEX_PATTERN: str = "^\\d\\d:\\d\\d:\\d\\d$"
+    _TIME_REGEX_PATTERN: str = "^(\\d?\\d):(\\d\\d):(\\d\\d).*"
     _TIME_REGEX: re.Pattern[anystr] = re.compile(_TIME_REGEX_PATTERN)
     regex_match = _TIME_REGEX.match(time_str)
     if regex_match is None:
@@ -18,9 +18,10 @@ def time_to_duration(time_str: str) -> int:
             f"Provided time string:\n\t{time_str}\nIs not a valid time string"
         )
 
-    hours = int(time_str[0:2])
-    minutes = int(time_str[3:5])
-    seconds = int(time_str[6:8])
+    times = regex_match.groups()
+    hours = int(times[0])
+    minutes = int(times[1])
+    seconds = int(times[2])
 
     duration: int = (hours * 60 * 60) + (minutes * 60) + (seconds)
     return duration
