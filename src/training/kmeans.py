c9b10892622c2afe01135904fc53d6efb250451e
Sean Allesina-McGrory <allesis@wwu.edu>
2026-02-26 16:20:22 -0800

    Update KMeans model to inherit from base model
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- src/training/kmeans.py
+++ src/training/kmeans.py
@@ -1,32 +1,13 @@
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans as Sk_KMeans
+from training.model import Model
 import type_enforced
 
 
 @type_enforced.Enforcer(enabled=True, strict=True, clean_traceback=True)
-class KMeans:
+class KMeans(Model):
     @type_enforced.Enforcer(enabled=True, strict=True, clean_traceback=True)
-    def __init__(**kwargs):
+    def __init__(self: KMeans, n_clusters: int = 2):
         """Initialize the model with the values provided in kwargs
         Defaults should be defined so that if no kwargs are provided, the model functions correctly
         """
-        pass
-
-    @type_enforced.Enforcer(enabled=True, strict=True, clean_traceback=True)
-    def train(source: list[float], target: list[float]):
-        """Train the model to predict source -> target"""
-        pass
-
-    @type_enforced.Enforcer(enabled=True, strict=True, clean_traceback=True)
-    def evaluate(source: list[float], target: list[float]) -> float:
-        """Evaluate the model's ability to predict source -> target.
-        The model will train itself it is has not been prior to this method executing.
-        Returns the accuracy as a float
-        """
-        pass
-
-    @type_enforced.Enforcer(enabled=True, strict=True, clean_traceback=True)
-    def predict(source: list[float]) -> list[float]:
-        """Predicts the values of source, returning the predictions.
-        The model will train itself it is has not been prior to this method executing.
-        """
-        pass
+        self._model: Sk_KMeans = Sk_KMeans(n_clusters=n_clusters)
